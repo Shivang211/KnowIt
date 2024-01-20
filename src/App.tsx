a763138d9f8847a154704d66cf85bb67d41abc08
shivang211 <shivanggupta2110@gmail.com>
2024-01-20 17:18:18 +0800

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/Homepage";
+import ScorePage from "./pages/ScorePage";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/scorepage" element={<ScorePage />} />
       </Routes>
     </>
   );

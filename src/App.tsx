1e61bd5b0d2bc8f35055895c1cb7700ce7dcdd6e
dedsecrattle <itsprabxxx@gmail.com>
2024-01-20 17:12:05 +0800

    Added ScorePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { Typography } from "@mui/material";
-
 function App() {
   return (
     <>
-      <Typography sx={{ fontWeight: "bolder", fontFamily: "Gralliec" }} variant={"h4"} gutterBottom>
-        This is a box!
-      </Typography>
     </>
   );
 }
